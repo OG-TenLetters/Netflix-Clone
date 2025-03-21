b764e24ddb48a7289f1937ec61e9f798f25c31f0
Jadon <1tenletters0@gmail.com>
2025-03-21 14:33:32 -0700

    I'm struggling to get the API to cooperate

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,29 +1,55 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./TitleCards.css"
 import cards__data from '../../assets/cards/Cards_data'
-
-
+import axios from 'axios'
 
 const TitleCards = ({title, category}) => {
+
+  const [apiData, setApiData] = useState([])
+
   const cardsRef =useRef()
 
-const handleWheel = (event) => {
-  event.preventDefault();
-  cardsRef.current.scrollLeft += event.deltaY;
-}
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZmJiNjlkYWJmMmYxNTQ4ZGE3MWFkZjQ5NGRlMjVhMiIsIm5iZiI6MTc0MjU4NTA2NC45MDYsInN1YiI6IjY3ZGRiY2U4MDUxY2JhOTA2NmY1NjRlNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oyAsQcXmbm9BoxH4RYdKZzfKfDbbOcAY_aYJe8dhDiM'
+    }
+  };
+  
 
-useEffect(() => {
+
+
+  
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  }
+  
+  useEffect(() => {
+
+  //   fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+  // .then(res => res.json())
+  // .then(res => console.log(res))
+  // .catch(err => console.error(err)); //The Recommended way which made the whole screen turn black and console errors of .map not defined
+    
+    async function fetchMovies() { //an attempt to make it work
+      const { data } = await axios.get('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+      console.log(data.results)
+      const apiRes = data.results
+    }
+  fetchMovies(),
   cardsRef.current.addEventListener('wheel', handleWheel)
-},[])
+},[]) 
   return (
     <div className='title__cards'>
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card__list" ref={cardsRef}>
-        {cards__data.map((card, index) => {
+        {apiData.map((card, index) => { //Issues as soon as I switch from cards__data to apiData
           return (
             <div className="card" key={index}>
-              <img src={card.image} alt="" />
-              <p>{card.name}</p>
+              <img src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path} alt="" />
+              <p>{card.original_title}</p>
             </div>
           )
         })}
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import "./Login.css"
-import Logo from '../../assets/logo.png'
+import React, { useState } from "react";
+import "./Login.css";
+import Logo from "../../assets/logo.png";
 
 const Login = () => {
+  const [signState, setSignState] = useState("Sign In");
+
   return (
-    <div className='login'>
-      <img src={Logo} className='login__logo' alt="" />
+    <div className="login">
+      <img src={Logo} className="login__logo" alt="" />
       <div className="login__form">
-        <h1>Sign Up</h1>
+        <h1>{signState}</h1>
         <form>
-          <input type="text" placeholder='Your Name' />
-          <input type="email" placeholder='Email' />
-          <input type="password" placeholder='Password' />
-          <button>Sign Up</button>
+          {signState === "Sign Up" ? (
+            <input type="text" placeholder="Your name" />
+          ) : (
+            <></>
+          )}
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>{signState}</button>
           <div className="form__help">
             <div className="remember">
               <input type="checkbox" />
@@ -20,11 +26,32 @@ const Login = () => {
             </div>
             <p>Need Help?</p>
           </div>
-          
         </form>
+        <div className="form__switch">
+          {signState === "Sign In" ? (
+            <p>
+              New To Netflix?{" "}
+              <span
+                onClick={() => {
+                  setSignState("Sign Up");
+                }}
+              >
+                Sign Up Now
+              </span>
+            </p>
+          ) : (
+            <p>
+              Already Have An Account? <span
+               onClick={() => {
+                setSignState("Sign In");
+              }}
+              >Sign In Now</span>
+            </p>
+          )}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
