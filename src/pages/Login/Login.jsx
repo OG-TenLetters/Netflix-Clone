e2e04d92b34850eb69b1b6b837c4dd845214beb2
Jadon <1tenletters0@gmail.com>
2025-03-21 11:10:30 -0700

    Sign Up form completed on Login, moving to making it dynamic with Sign In

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import "./Login.css"
+import Logo from '../../assets/logo.png'
+
 const Login = () => {
   return (
-    <div className='login'>Login</div>
+    <div className='login'>
+      <img src={Logo} className='login__logo' alt="" />
+      <div className="login__form">
+        <h1>Sign Up</h1>
+        <form>
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email' />
+          <input type="password" placeholder='Password' />
+          <button>Sign Up</button>
+          <div className="form__help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+          
+        </form>
+      </div>
+    </div>
   )
 }
 
